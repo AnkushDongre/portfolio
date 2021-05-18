ab41feb997c002fee59c5293a48977a1171e1df0
AnkushDongre <dongreankush51@gmail.com>
2021-05-18 18:25:50 +0530

    nominal bug update

---
+++ script.js
@@ -0,0 +1,5 @@
+// SVG Graphic from https://undraw.co/illustrations and customised in Adobe Illustrator 
+
+// Example project logos made with Canva for free
+
+// I created the weird blue and yellow circles with Adobe XD
